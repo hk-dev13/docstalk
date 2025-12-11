65dc51cbd9dfc2bbdc3cfff40075fc3a6444cc98
Husni Kusuma <husnikusuma00@gmail.com>
2025-12-11 18:06:27 +0700

    fix(db): Add base_url to cloud doc_sources migration

--- docs/database/migrations/010_add_cloud_doc_sources.sql
+++ docs/database/migrations/010_add_cloud_doc_sources.sql
@@ -5,24 +5,32 @@
 -- that are referenced in ecosystems but missing from doc_sources table
 
 -- Add cloud provider doc sources
-INSERT INTO doc_sources (id, name, description, is_active, ecosystem_id, keywords) VALUES
-  ('azure', 'Azure', 'Microsoft Azure cloud platform documentation', true, 'cloud_infra', 
+INSERT INTO doc_sources (id, name, description, base_url, is_active, ecosystem_id, keywords) VALUES
+  ('azure', 'Azure', 'Microsoft Azure cloud platform documentation', 
+   'https://learn.microsoft.com/en-us/azure/', true, 'cloud_infra', 
    ARRAY['azure', 'microsoft', 'cloud', 'vm', 'app service', 'cosmos db', 'blob storage']),
-  ('aws', 'AWS', 'Amazon Web Services cloud platform documentation', true, 'cloud_infra',
+  ('aws', 'AWS', 'Amazon Web Services cloud platform documentation', 
+   'https://docs.aws.amazon.com/', true, 'cloud_infra',
    ARRAY['aws', 'amazon', 's3', 'ec2', 'lambda', 'dynamodb', 'cloudfront', 'iam']),
-  ('gcp', 'GCP', 'Google Cloud Platform documentation', true, 'cloud_infra',
+  ('gcp', 'GCP', 'Google Cloud Platform documentation', 
+   'https://cloud.google.com/docs', true, 'cloud_infra',
    ARRAY['gcp', 'google cloud', 'bigquery', 'cloud run', 'cloud functions', 'gke', 'firestore']),
-  ('firebase', 'Firebase', 'Firebase platform documentation', true, 'cloud_infra',
+  ('firebase', 'Firebase', 'Firebase platform documentation', 
+   'https://firebase.google.com/docs', true, 'cloud_infra',
    ARRAY['firebase', 'firestore', 'realtime database', 'auth', 'fcm', 'hosting']),
-  ('kubernetes', 'Kubernetes', 'Container orchestration documentation', true, 'cloud_infra',
+  ('kubernetes', 'Kubernetes', 'Container orchestration documentation', 
+   'https://kubernetes.io/docs/', true, 'cloud_infra',
    ARRAY['kubernetes', 'k8s', 'kubectl', 'pod', 'deployment', 'service', 'ingress', 'helm']),
-  ('redis', 'Redis', 'In-memory data store documentation', true, 'database',
+  ('redis', 'Redis', 'In-memory data store documentation', 
+   'https://redis.io/docs/', true, 'database',
    ARRAY['redis', 'cache', 'pub/sub', 'streams', 'cluster', 'sentinel']),
-  ('mongodb', 'MongoDB', 'NoSQL document database documentation', true, 'database',
+  ('mongodb', 'MongoDB', 'NoSQL document database documentation', 
+   'https://www.mongodb.com/docs/', true, 'database',
    ARRAY['mongodb', 'mongo', 'nosql', 'document', 'aggregation', 'mongoose'])
 ON CONFLICT (id) DO UPDATE SET
   name = EXCLUDED.name,
   description = EXCLUDED.description,
+  base_url = EXCLUDED.base_url,
   ecosystem_id = EXCLUDED.ecosystem_id,
   keywords = EXCLUDED.keywords;
 
